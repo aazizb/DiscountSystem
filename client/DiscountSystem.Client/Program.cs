14c589aab02d0d1bc525398f0642622cd2ed83f7
a.a.barry <aazizb@hotmail.com>
2025-10-25 18:14:23 +0200

    Added deployment on docker containers

--- client/DiscountSystem.Client/Program.cs
+++ client/DiscountSystem.Client/Program.cs
@@ -3,23 +3,27 @@
 using Grpc.Net.Client;
 
 // Enable HTTP/3 support in client
-var address = "https://localhost:5001";
+var address = "https://server:5001";
 var channel = GrpcChannel.ForAddress(address, new GrpcChannelOptions
 {
     HttpHandler = new SocketsHttpHandler
     {
-        EnableMultipleHttp3Connections = true
+        EnableMultipleHttp3Connections = true ,
+        SslOptions = new System.Net.Security.SslClientAuthenticationOptions
+        {
+            RemoteCertificateValidationCallback = (sender, certificate, chain, sslPolicyErrors) => true
+        }
     }
 });
 var client = new DiscountService.DiscountServiceClient(channel);
 
 // Generate codes
-int requestedCount = 2200;
-int codeLength = 8;
+int requestedCount = 20;
+int codeLength = 7;
 var genResponse = await client.GenerateDiscountCodesAsync(new GenerateDiscountCodeRequest { Count = requestedCount, Length = codeLength });
 Console.WriteLine($"Generation success: {genResponse.Result}");
 
 // Use a discount code, update codeToUse with good/actual code
-string codeToUse = " ";
+string codeToUse = "F2DFGSJ";
 var useResponse = await client.UseDiscountCodeAsync(new UseDiscountCodeRequest { Code = codeToUse });
 Console.WriteLine($"Use result: {useResponse.Result}");
--- src/DiscountSystem.Server/Program.cs
+++ src/DiscountSystem.Server/Program.cs
@@ -4,15 +4,32 @@ using DiscountSystem.Infrastructure.Repositories;
 using DiscountSystem.Server.Services;
 using DiscountSystem.Services;
 
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddGrpc();
+
+// Configure DbContext with retry logic
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+var maxRetryCount = builder.Configuration.GetValue<int>("Database:MaxRetryCount", 10);
+var maxRetryDelay = builder.Configuration.GetValue<int>("Database:MaxRetryDelay", 30);
+
 builder.Services.AddDbContext<DiscountCodeDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"),
-    o => o.MigrationsAssembly("DiscountSystem.Server")));
+{
+    options.UseSqlServer(connectionString, sqlOptions =>
+    {
+        sqlOptions.EnableRetryOnFailure(
+            maxRetryCount: maxRetryCount,
+            maxRetryDelay: TimeSpan.FromSeconds(maxRetryDelay),
+            errorNumbersToAdd: null);
+        sqlOptions.CommandTimeout(60);
+
+        sqlOptions.MigrationsAssembly("DiscountSystem.Server");
+    });
+});
 
 builder.Services.AddScoped<IDiscountCodeService, DiscountCodeService>();
 builder.Services.AddScoped<IDiscountCodeRepository, DiscountCodeRepository>();
@@ -22,20 +39,23 @@ builder.Services.AddScoped<IDiscountCodeGenerator, DiscountCodeGenerator>();
 int intPort = 5001; // Default port for HTTPS
 builder.WebHost.ConfigureKestrel(options =>
 {
-    options.ListenLocalhost(intPort, listenOptions =>
+    options.ListenAnyIP(intPort, listenOptions => 
     {
-        listenOptions.Protocols = Microsoft.AspNetCore.Server.Kestrel.Core.HttpProtocols.Http1AndHttp2AndHttp3;
-        listenOptions.UseHttps(); // HTTP/3 requires HTTPS
+        listenOptions.Protocols = HttpProtocols.Http1AndHttp2AndHttp3;
+        listenOptions.UseHttps();
     });
 });
 
 var app = builder.Build();
-
+app.UseHsts();
 // apply migrations on startup (optional)
-using (var scope = app.Services.CreateScope())
+if (app.Environment.IsDevelopment())
 {
-    var db = scope.ServiceProvider.GetRequiredService<DiscountCodeDbContext>();
-    db.Database.Migrate();
+    using (var scope = app.Services.CreateScope())
+    {
+        var db = scope.ServiceProvider.GetRequiredService<DiscountCodeDbContext>();
+        db.Database.Migrate();
+    }
 }
 // Configure the HTTP request pipeline.
 app.MapGrpcService<DiscountCodeServices>();
